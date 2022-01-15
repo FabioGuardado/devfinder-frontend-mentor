3496dbe425a3fd7c3faa462fa8f0e96e605630d9
Fabio Guardado <fabio.guardado2012@gmail.com>
2022-01-14 21:38:29 -0600

    :hammer: Add AppContainer component

---
+++ src/components/AppContainer/AppContainer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import SearchBar from '../SearchBar/SearchBar';
+import UserSection from '../UserSection/UserSection';
+
+const AppContainer: React.FunctionComponent = () => {
+  return (
+    <>
+      <SearchBar />
+      <UserSection />
+    </>
+  );
+};
+
+export default AppContainer;

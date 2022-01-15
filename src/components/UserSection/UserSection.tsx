a83d57b726ec608cd5886eba14f5181f0e9d73bb
Fabio Guardado <fabio.guardado2012@gmail.com>
2022-01-14 21:38:01 -0600

    :hammer: Add UserSection component

---
+++ src/components/UserSection/UserSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserSection: React.FunctionComponent = () => {
+  return (
+    <div>
+      <h1>userSection</h1>
+    </div>
+  );
+};
+
+export default UserSection;

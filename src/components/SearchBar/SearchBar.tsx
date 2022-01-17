672f103a5530ddabd730c45a8ae4468ab4a189be
Fabio Guardado <fabio.guardado2012@gmail.com>
2022-01-17 16:04:14 -0600

    :hammer: Update SearchBar form logic

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 import './SearchBar.scss';
+import SearchBarProps from '../../types/SearchBarProps';
 
-const SearchBar: React.FunctionComponent = () => {
+const SearchBar: React.FunctionComponent<SearchBarProps> = ({
+  setUsername,
+}) => {
+  const [textInput, setTextInput] = useState<string>('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTextInput(e.target.value);
+  };
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    if (textInput.trim().length > 0) setUsername(textInput);
+  };
+
   return (
     <div className="search-bar">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="search-bar__group">
           <div className="search-bar__input-container">
             <label className="search-bar__label" htmlFor="search-bar__input">
@@ -18,9 +32,12 @@ const SearchBar: React.FunctionComponent = () => {
               id="search-bar__input"
               type="text"
               placeholder="Search GitHub username..."
+              onChange={handleChange}
             />
           </div>
-          <button className="search-bar__btn--submit">Search</button>
+          <button type="submit" className="search-bar__btn--submit">
+            Search
+          </button>
         </div>
       </form>
     </div>

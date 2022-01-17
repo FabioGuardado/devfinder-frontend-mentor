9f5ca613369727dd0817d6ba86e395bd3c9fd73a
Fabio Guardado <fabio.guardado2012@gmail.com>
2022-01-17 17:21:42 -0600

    :hammer: Update UserSection component to make it able to fetch and use real GitHub data

--- src/components/UserSection/UserSection.tsx
+++ src/components/UserSection/UserSection.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faMapMarkerAlt,
@@ -8,17 +8,31 @@ import {
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 import './UserSection.scss';
-import defaultPicture from '../../assets/defaultPicture.png';
 import UserSectionProps from '../../types/UserSectionProps';
+import IGitHubUser from '../../interfaces/IGitHubUser';
+import useIsMounted from '../../hooks/useIsMounted';
+import { getUserByUsername } from '../../API/users';
 
 const UserSection: React.FunctionComponent<UserSectionProps> = ({
   username,
 }) => {
-  return (
+  const [userDetails, setUserDetails] = useState<null | IGitHubUser>(null);
+  const isMounted = useIsMounted();
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await getUserByUsername(username ? username : 'octocat');
+      if (isMounted()) setUserDetails(response);
+    };
+
+    getData();
+  }, [username]);
+
+  return userDetails ? (
     <div className="app-container">
       <div className="app-container__picture-section">
         <img
-          src={defaultPicture}
+          src={userDetails.avatar_url}
           alt="Profile"
           className="app-container__image"
         />
@@ -26,66 +40,103 @@ const UserSection: React.FunctionComponent<UserSectionProps> = ({
       <div className="app-container__information-section">
         <div className="app-container__information-header">
           <div className="app-container__information-header__first-col">
-            <h1>The Octocat</h1>
+            <h1>{userDetails.name}</h1>
             <span>
-              <a href="#">@octocat</a>
+              <a
+                href={`https://github.com/${userDetails.login}`}
+                target="_blank"
+              >
+                @{userDetails.login}
+              </a>
             </span>
           </div>
           <div className="app-container__information-header__second-col">
-            <h3>Joined 25 Jan 2011</h3>
+            <h3>Joined {userDetails.created_at}</h3>
           </div>
         </div>
 
         <div className="app-container__bio">
-          <p>This profile has no bio</p>
+          <p>{userDetails.bio ? userDetails.bio : 'This profile has no bio'}</p>
         </div>
 
         <div className="app-container__stats">
           <div className="app-container__stats__stats-group">
             <span className="app-container__stats__title">Repos</span>
-            <h3 className="app-container__stats__data">8</h3>
+            <h3 className="app-container__stats__data">
+              {userDetails.public_repos}
+            </h3>
           </div>
           <div className="app-container__stats__stats-group">
             <span className="app-container__stats__title">Followers</span>
-            <h3 className="app-container__stats__data">3938</h3>
+            <h3 className="app-container__stats__data">
+              {userDetails.followers}
+            </h3>
           </div>
           <div className="app-container__stats__stats-group">
             <span className="app-container__stats__title">Following</span>
-            <h3 className="app-container__stats__data">9</h3>
+            <h3 className="app-container__stats__data">
+              {userDetails.following}
+            </h3>
           </div>
         </div>
 
         <div className="app-container__additional-information">
-          <div className="app-container__additional-information__item">
+          <div
+            className={`app-container__additional-information__item ${
+              !userDetails.location && 'disabled'
+            }`}
+          >
             <span className="app-container__additional-information__item__icon">
               <FontAwesomeIcon icon={faMapMarkerAlt} />
             </span>
-            <span>San Francisco</span>
+            <span>
+              {userDetails.location ? userDetails.location : 'Not available'}
+            </span>
           </div>
 
-          <div className="app-container__additional-information__item disabled">
+          <div
+            className={`app-container__additional-information__item ${
+              !userDetails.twitter_username && 'disabled'
+            }`}
+          >
             <span className="app-container__additional-information__item__icon">
               <FontAwesomeIcon icon={faTwitter} />
             </span>
-            <span>Not available</span>
+            <span>
+              {userDetails.twitter_username
+                ? `@${userDetails.twitter_username}`
+                : 'Not available'}
+            </span>
           </div>
 
-          <div className="app-container__additional-information__item">
+          <div
+            className={`app-container__additional-information__item ${
+              !userDetails.blog && 'disabled'
+            }`}
+          >
             <span className="app-container__additional-information__item__icon">
               <FontAwesomeIcon icon={faLink} />
             </span>
-            <span>https://github.blog</span>
+            <span>{userDetails.blog ? userDetails.blog : 'Not available'}</span>
           </div>
 
-          <div className="app-container__additional-information__item">
+          <div
+            className={`app-container__additional-information__item ${
+              !userDetails.company && 'disabled'
+            }`}
+          >
             <span className="app-container__additional-information__item__icon">
               <FontAwesomeIcon icon={faBuilding} />
             </span>
-            <span>@github</span>
+            <span>
+              {userDetails.company ? `${userDetails.company}` : 'Not available'}
+            </span>
           </div>
         </div>
       </div>
     </div>
+  ) : (
+    <h1>Loading...</h1>
   );
 };
 

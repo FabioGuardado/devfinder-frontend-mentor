9ce1d3138a0f51857b93e353764b9305504f65a5
Fabio Guardado <fabio.guardado2012@gmail.com>
2022-01-23 13:41:15 -0600

    :hammer: Add dark mode toggler functionality

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import useDarkMode from '../../hooks/useDarkMode';
 import './Header.scss';
 
 const Header: React.FunctionComponent = () => {
+  const { toggleDarkMode, isCurrentlyDark } = useDarkMode();
+
+  const handleCheck = () => {
+    toggleDarkMode(!isCurrentlyDark);
+  };
+
   return (
     <div className="header">
       <h1 className="header__brand">devfinder</h1>
-      <div className="header__theme-switcher">Light</div>
+      <div>
+        <label htmlFor="theme">{isCurrentlyDark ? 'Light' : 'Dark'}</label>
+        <input id="theme" name="theme" type="checkbox" onClick={handleCheck} />
+      </div>
     </div>
   );
 };

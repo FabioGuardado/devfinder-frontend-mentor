eb30b6245c086570c05693d0f4b9b3505ab360b3
Fabio Guardado <fabio.guardado2012@gmail.com>
2022-01-23 17:51:09 -0600

    :recycle: Refactor Switch component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import useDarkMode from '../../hooks/useDarkMode';
 import './Header.scss';
+import Switch from './Switch/Switch';
 
 const Header: React.FunctionComponent = () => {
-  const { toggleDarkMode, isCurrentlyDark } = useDarkMode();
-
-  const handleCheck = () => {
-    toggleDarkMode(!isCurrentlyDark);
-  };
-
   return (
     <div className="header">
       <h1 className="header__brand">devfinder</h1>
-      <div>
-        <label htmlFor="theme">{isCurrentlyDark ? 'Light' : 'Dark'}</label>
-        <input id="theme" name="theme" type="checkbox" onClick={handleCheck} />
-      </div>
+      <Switch />
     </div>
   );
 };

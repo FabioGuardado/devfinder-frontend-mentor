fb91f12c32aefaec3825f5e3bc69cbb7631bb51e
Fabio Guardado <fabio.guardado2012@gmail.com>
2022-01-29 11:14:58 -0600

    :hammer: Add condition to throw an error in case of API error response

--- src/helpers/fetchService.ts
+++ src/helpers/fetchService.ts
@@ -2,6 +2,16 @@ async function http<T>(path: string, config: RequestInit): Promise<T> {
   const request = new Request(path, config);
   const response = await fetch(request);
 
+  if (!response.ok) {
+    throw {
+      message:
+        response.status === 404
+          ? 'The user that you were searching was not found.'
+          : 'Something ocurred while getting data.',
+      errorCode: response.status,
+    };
+  }
+
   const data = await response.json();
 
   return data;
